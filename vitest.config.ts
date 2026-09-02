158051e3b5ab1000718acf43d2794395fa309844
Rotem Dan <rotemdan@gmail.com>
2026-09-02 15:39:44 +0300

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, configDefaults } from 'vitest/config'
 export default defineConfig({
 	test: {
 		reporters: ['default'],
-		disableConsoleIntercept: true,
+		disableConsoleIntercept: false,
 
 		include: ['tests/**/*.test.ts'],
 
